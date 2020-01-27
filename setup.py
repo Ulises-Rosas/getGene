e6b3494bf3da51525891a4a50f7c7a212a2611c5
Ulises-Rosas <ulisesfrosasp@gmail.com>
2020-01-27 17:56:45 -0500

    delete this

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(name="genetable",
+      version='0.1',
+      author='Ulises Rosas',
+      long_description = readme,
+      long_description_content_type = 'text/markdown',
+      author_email='ulisesfrosasp@gmail.com',
+      url='https://github.com/Ulises-Rosas/geneTable',
+      packages = ['genetable'],
+      package_dir = {'genetable': 'src'},
+      scripts = ['src/geneTable.py'],
+      classifiers = [
+             'Programming Language :: Python :: 3',
+             'License :: OSI Approved :: MIT License'
+             ]
+     )
